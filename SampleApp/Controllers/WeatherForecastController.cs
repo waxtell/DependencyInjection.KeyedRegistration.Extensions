9a9e6a09ef6a654e3f5f7cdc93893b4bb2dd2f7e
Will Axtell <waxtell@transtar1.com>
2020-07-18 20:22:55 -0400

    Added to the example application.

--- SampleApp/Controllers/WeatherForecastController.cs
+++ SampleApp/Controllers/WeatherForecastController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using DependencyInjection.KeyedRegistration.Extensions.Extensions;
 
 namespace SampleApp.Controllers
 {
@@ -11,28 +8,47 @@ namespace SampleApp.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        // This is most assuredly a horrible example.  Please understand this code is intended
+        // solely to demonstrate the setup and use of the package.
 
-        private readonly ILogger<WeatherForecastController> _logger;
-        private readonly Func<int, IWeatherForecast> _resolver;
+        // The following block of code utilizes the native IServiceProvider with the
+        // GetService extension that accepts an instance name (key)
+        private readonly IServiceProvider _serviceProvider;
 
-        public WeatherForecastController(Func<int, IWeatherForecast> resolver, ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(IServiceProvider serviceProvider)
         {
-            _resolver = resolver;
-            _logger = logger;
+            _serviceProvider = serviceProvider;
         }
 
         [HttpGet]
         public IWeatherForecast Get()
         {
-            var zipCodes = new[] {44067, 44143, 44144};
+            var zipCodes = new[] { 44067, 44143, 44144 };
+
+            var rng = new Random();
+
+            return _serviceProvider.GetService<int,IWeatherForecast>(zipCodes[rng.Next(0, 3)]);
+        }
+/*
+        // The following block of code demonstrates the use of a resolver.  Please be sure to
+        // register your resolver!
+
+        private readonly Func<int, IWeatherForecast> _resolver;
+
+        public WeatherForecastController(Func<int, IWeatherForecast> resolver)
+        {
+            _resolver = resolver;
+        }
+
+        [HttpGet]
+        public IWeatherForecast Get()
+        {
+            var zipCodes = new[] { 44067, 44143, 44144 };
 
             var rng = new Random();
 
             return _resolver.Invoke(zipCodes[rng.Next(0, 3)]);
         }
+*/
     }
 }
